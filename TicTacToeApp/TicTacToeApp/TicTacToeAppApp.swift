b58e177c98163df9c01c94de3a64ef611024580e
Raj Anand <raj.anand0908@gmail.com>
2023-01-14 19:59:22 +0530

    [TASK] Added Project

---
+++ TicTacToeApp/TicTacToeApp/TicTacToeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeAppApp.swift
+//  TicTacToeApp
+//
+//  Created by Raj Anand on 14/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
