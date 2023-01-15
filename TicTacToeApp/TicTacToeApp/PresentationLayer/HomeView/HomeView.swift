fc0eae3706e3e9389f43a199720210b7718a2a1f
Raj Anand <raj.anand0908@gmail.com>
2023-01-15 12:52:09 +0530

    [TASK] Implemented home screen

---
+++ TicTacToeApp/TicTacToeApp/PresentationLayer/HomeView/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  TicTacToeApp
+//
+//  Created by Raj Anand on 15/01/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    HomeView()
+  }
+}
