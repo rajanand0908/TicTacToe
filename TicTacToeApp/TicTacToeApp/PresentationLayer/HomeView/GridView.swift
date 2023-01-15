d7fbcacfca3ff6f3ba19b7ec4c7cf5478269b7ee
Raj Anand <raj.anand0908@gmail.com>
2023-01-15 19:56:48 +0530

    [TASK] Implemented win functionality

--- TicTacToeApp/TicTacToeApp/PresentationLayer/HomeView/GridView.swift
+++ TicTacToeApp/TicTacToeApp/PresentationLayer/HomeView/GridView.swift
@@ -35,12 +35,18 @@ struct GridView: View {
               guard !isSquareOccupied(in: moves, forIndex: index) else { return }
               moves[index] = Move(player: .human,
                                   boardIndex: index)
+              if checkWinCondition(for: .human, in: moves) {
+                print("Human wins")
+              } 
               isGameBoardDisabled = true
               // check for win condition or draw
               DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                 let computerPosition = determineComputerMovePosition(in: moves)
                 moves[computerPosition] = Move(player: .computer, boardIndex: computerPosition)
                 isGameBoardDisabled = false
+              }
+              if checkWinCondition(for: .computer, in: moves) {
+                print("Computer wins")
               }
             }
           }
@@ -62,6 +68,12 @@ struct GridView: View {
       movePoisiton = Int.random(in: 0..<9)
     }
     return movePoisiton
+  }
+  
+  func checkWinCondition(for player: Player, in moves: [Move?]) -> Bool {
+    let winPatters: Set<Set<Int>> = [[0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7], [2, 5, 8], [2, 4, 6], [3, 4, 5], [6, 7, 8]]
+    let playersPosition = Set(moves.compactMap { $0 }.filter { $0.player == player }.map { $0.boardIndex })
+    return winPatters.contains(playersPosition)
   }
   
 }
